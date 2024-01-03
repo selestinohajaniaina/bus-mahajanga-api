7f338735d71a8c1ae747df152b7f0be1e357f2c0
selestino <selestinoolivier@gmail.com>
2024-01-03 18:09:12 +0300

    update:readme

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,9 @@
 // index.d.ts
 
 declare module 'bus-mj' {
-    export function getBus(arg1: string, arg2: string): any; // Remplacez 'any' par le type approprié
-    export function getAllBus(): any; // Remplacez 'any' par le type approprié
-    export function getAllStop(): any; // Remplacez 'any' par le type approprié
-    export function getStop(arg1: number): any; // Remplacez 'any' par le type approprié
+    export function getBus(arg1: string, arg2: string): [{}];
+    export function getAllBus(): [{}];
+    export function getAllStop(): [{}];
+    export function getStop(arg1: number): [{}];
   }
   
