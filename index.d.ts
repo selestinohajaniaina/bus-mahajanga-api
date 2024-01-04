f8f5c48e0b44c0be8aee4c2742317fa9f2268169
selestino <selestinoolivier@gmail.com>
2024-01-04 19:34:52 +0300

    update:index.d.ts adding typing

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,18 @@
 // index.d.ts
 
 declare module 'bus-mj' {
-    export function getBus(arg1: string, arg2: string): [{}];
-    export function getAllBus(): [{}];
-    export function getAllStop(): [{}];
-    export function getStop(arg1: number): [{}];
-  }
-  
+  export function getBus(arg1: string, arg2: string): [{
+    BUS_ID: number,
+  BUS_NAME: string,
+  BUS_PLAQUE: string,
+  LONG: string,
+  TIME: string,
+  YOUR_TRAJET: []
+  }];
+  export function getAllBus(): [{
+    BUS_ID: number,
+    BUS_NAME: string
+  }];
+  export function getAllStop(): [];
+  export function getStop(arg1: number): [];
+}
