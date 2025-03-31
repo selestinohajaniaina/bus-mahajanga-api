21783dd989a1ca03cebe80ada8d331d166cbbfd4
selestino <selestinoolivier@gmail.com>
2025-03-31 16:05:53 +0300

    @selestinohajaniaina [v1.2.1] add new data type of bus data description

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,7 @@ declare module 'bus-mj' {
     color: string,
     band: string[]
   }];
-  export function busAt(busId: string): {label: string,color: string,band: string[]};
+  export function busAt(busId: string): {label: string,color: string,band: string[], board: string | null};
   export function getAllStop(): { key: string, value: string }[];
   export function getStop(busId: string): string[];
   export function getStopLabel(stopId: string): string;
